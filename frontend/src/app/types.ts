1e206d074af4447638fc26ad6cb10507ba0a2b77
Kimberly Brown <kjbrown.home@gmail.com>
2025-03-26 20:11:10 -0600

    Semi-working form and displays triggers

---
+++ frontend/src/app/types.ts
@@ -0,0 +1,53 @@
+export type TimeAction =
+   | "reaction"
+   | "bonus action"
+   | "action"
+   | "movement"
+   | "free action"
+   | "short rest"
+   | "long rest"
+   | "other";
+
+export type DamageType =
+   | "bludgeoning"
+   | "piercing"
+   | "slashing"
+   | "fire"
+   | "cold"
+   | "lightning"
+   | "thunder"
+   | "poison"
+   | "acid"
+   | "psychic"
+   | "necrotic"
+   | "radiant"
+   | "force"
+   | "other";
+
+export type CommonTrigger =
+   | TimeAction
+   | "enemy leaves range"
+   | "enemy casts spell";
+
+export interface Range {
+   normal: number;
+   long?: number;
+}
+
+export interface Attack {
+   damage: string;
+   type: DamageType;
+   range?: Range;
+}
+
+export interface Action {
+   title: string;
+   description?: string;
+   time?: TimeAction;
+   attack?: Attack;
+   triggers: (CommonTrigger | string)[];
+}
+
+export interface CharacterData {
+   actions: Action[];
+}
